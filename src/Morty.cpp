fc23fce66d77a8f38857473d218cb06b2c8045b7
ahmadeladawy2001 <70240901+ahmadeladawy2001@users.noreply.github.com>
2020-09-27 00:05:47 +0200

    Update Morty.cpp

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -13,9 +13,82 @@
 // =========================================================================== // 
 //          Define functions enclosed in Namespace C137 here 
 // =========================================================================== // 
+namespace C137 {
+    void Morty(int start, int stop) {
+        for (int i = start; i <= stop; i++) {
+            if (i % 15 == 0) {
+                cout << i << ": Aww Geez Man" << endl;
+            }
+            else if (i % 3 == 0) {
+                cout << i << ": Aww" << endl;
+            }
+            else if (i % 5 == 0) {
+                cout << i << ": Geez" << endl;
+            }
+            else {
+                cout << i << ":" << endl;
+            }
+        }
+    }
+
+    void Morty(int start, int stop, int step) {
+        for (int i = start; i <= stop; i = i + step) {
+            if (i % 15 == 0) {
+                cout << i << ": Aww Geez Man" << endl;
+            }
+            else if (i % 3 == 0) {
+                cout << i << ": Aww" << endl;
+            }
+            else if (i % 5 == 0) {
+                cout << i << ": Geez" << endl;
+            }
+            else {
+                cout << i << ":" << endl;
+            }
+        }
+    }
+
+}
 
 
 
 // =========================================================================== // 
 //          Define functions enclosed in Namespace Z286 here 
 // =========================================================================== // 
+
+namespace Z286 {
+    void Morty(int start, int stop) {
+        for (int i = start; i <= stop; i++) {
+            if (i % 15 == 0) {
+                cout << i << ": naM zeeG wwA" << endl;
+            }
+            else if (i % 3 == 0) {
+                cout << i << ": wwA" << endl;
+            }
+            else if (i % 5 == 0) {
+                cout << i << ": zeeG" << endl;
+            }
+            else {
+                cout << i << ":" << endl;
+            }
+        }
+    }
+
+    void Morty(int start, int stop, int step) {
+        for (int i = start; i <= stop; i = i + step) {
+            if (i % 15 == 0) {
+                cout << i << ": naM zeeG wwA" << endl;
+            }
+            else if (i % 3 == 0) {
+                cout << i << ": wwA" << endl;
+            }
+            else if (i % 5 == 0) {
+                cout << i << ": zeeG" << endl;
+            }
+            else {
+                cout << i << ":" << endl;
+            }
+        }
+    }
+}
+
